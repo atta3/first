180e739fe516f8aa27f3abd1e976288da3bfe6b0
atta <web@atta-atta-atta.com>
2017-08-02 05:00:35 +0000

    sp header menu ok

--- menubutton.js
+++ menubutton.js
@@ -3,5 +3,13 @@
     $(document).ready(function(){
   $("#menubutton").click(function(){
     $(".sp_menu_toggle").slideToggle();
+        if ($(this).prevAll().is(':hidden')) {
+            $(this).hasClass('sp_menu_toggle').slideDown();
+            $(this).text('メニューを開く').addClass('close');
+        } else {
+            $(this).hasClass('sp_menu_toggle')();
+            $(this).text('閉じる').removeClass('close');
+        }
   });
 });
+
