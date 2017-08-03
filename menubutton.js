2c39df9314d9b476b6677fb26a6bf68454d3bbf1
atta <web@atta-atta-atta.com>
2017-08-03 13:31:05 +0000

    sp_sns_navi all ok

--- menubutton.js
+++ menubutton.js
@@ -2,12 +2,12 @@
     // ◇ボタンをクリックしたら、メニューが開きます
     $(document).ready(function(){
   $("#menubutton").click(function(){
-    $(".sp_menu_toggle").slideToggle();
+    $(".sp_menu_toggle,.sp_sns_navi").slideToggle();
         if ($(this).prevAll().is(':hidden')) {
-            $(this).hasClass('sp_menu_toggle').slideDown();
+            $(this).hasClass('sp_menu_toggle,sp_sns_navi').slideDown();
             $(this).text('メニューを開く').addClass('close');
         } else {
-            $(this).hasClass('sp_menu_toggle')();
+            $(this).hasClass('sp_menu_toggle,sp_sns_navi')();
             $(this).text('閉じる').removeClass('close');
         }
   });
